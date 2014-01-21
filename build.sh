6318bf43be66ecafc272d9b495f8ed70223ba1bf
megatron-uk <john@target-earth.net>
2014-01-21 11:55:17 +0000

    Simplified version of build script.

--- build.sh
+++ build.sh
@@ -1,19 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
 cd src
-echo "Starting compiler"
-huc -DDEBUG -s -02 test.c
-if [ "$?" == "0" ]
-then
-	echo "Compile successful - starting assembler"
-	pceas -s l0 test.s
-	if [ "$?" == "0" ]
-	then
-		echo "Assembly successful"
-	else
-		echo "Assembly not successful"
-	fi
-else
-	echo "Compile not successful"	
-fi
+huc -DDEBUG -s -O2 test.c && pceas -s -l0 test.s
 cd -
